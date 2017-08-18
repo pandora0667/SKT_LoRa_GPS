5b9beef7fa06286588cc95406b3bf3fdf3aa9aa4
seongwonlee <jusk2@naver.com>
2017-08-18 14:00:32 +0900

    code clean

--- LoRa_Send.js
+++ LoRa_Send.js
@@ -1,39 +1,38 @@
 const file = '/dev/ttyACM0';
-const file2 = '/dev/ttyACM1'; 
+const file2 = '/dev/ttyACM1';
 const SerialPort = require('serialport');
-const Angles = require('angles');
 const GPS = require('gps');
 const gps = new GPS;
 
 const port = new SerialPort(file, {
-		  baudrate: 4800,
-		  parser: SerialPort.parsers.readline('\r\n')
+    baudrate: 4800,
+    parser: SerialPort.parsers.readline('\r\n')
 });
 
 const LoRa = new SerialPort(file2, {
-		baudrate: 115200, 
-		parser: SerialPort.parsers.readline('\r\n')
-}); 
-console.log('----------    Trucker V1.0 LoRa Send GPS Starting...   ----------');  
+    baudrate: 115200,
+    parser: SerialPort.parsers.readline('\r\n')
+});
+console.log('----------    Trucker V1.0 LoRa Send GPS Starting...   ----------');
 let latitude = 0;
 let longitude = 0;
-gps.on('data', function(data) {
-			
-	if (gps.state.lat !== undefined && gps.state.lon !== undefined) {
-				latitude = gps.state.lat.toString(); 
-	            latitude = latitude.substring(0,7);
+gps.on(function () {
 
-				longitude = gps.state.lon.toString(); 
-				longitude = longitude.substring(0,9);
-	}
+    if (gps.state.lat !== undefined && gps.state.lon !== undefined) {
+        latitude = gps.state.lat.toString();
+        latitude = latitude.substring(0, 7);
+
+        longitude = gps.state.lon.toString();
+        longitude = longitude.substring(0, 9);
+    }
 });
 
-setInterval(function() {
-	let send = latitude + ',' + longitude;
-	LoRa.write(send);
-	console.log('LoRa Send : ' + send); 
-},5000);
+setInterval(function () {
+    let send = latitude + ',' + longitude;
+    LoRa.write(send);
+    console.log('LoRa Send : ' + send);
+}, 5000);
 
-port.on('data', function(data) {
-				gps.update(data);
+port.on('data', function (data) {
+    gps.update(data);
 });
--- TCP_Send.js
+++ TCP_Send.js
@@ -1,67 +1,65 @@
-const file = '/dev/ttyACM0';
-const file2 = '/dev/ttyACM1'; 
+const port = '/dev/ttyACM0';
 const SerialPort = require('serialport');
-const Angles = require('angles');
 const GPS = require('gps');
 const gps = new GPS;
 
-const net = require('net'); 
-const port = new SerialPort(file, {
-		  baudrate: 4800,
-		  parser: SerialPort.parsers.readline('\r\n')
+const net = require('net');
+const serialGPS = new SerialPort(port, {
+    baudrate: 4800,
+    parser: SerialPort.parsers.readline('\r\n')
 });
 
-function getConnection(connName){
-	let client = net.connect({port: 5001, host:'169.56.80.26'}, function() {
-			console.log(connName + ' Connected: ');
-			console.log('   local = %s:%s', this.localAddress, this.localPort);
-			console.log('   remote = %s:%s', this.remoteAddress, this.remotePort);
-			this.setEncoding('utf8');
+function getConnection(connName) {
+    let client = net.connect({port: 5001, host: 'localhost'}, function () {
+        console.log(connName + ' Connected: ');
+        console.log('   local = %s:%s', this.localAddress, this.localPort);
+        console.log('   remote = %s:%s', this.remoteAddress, this.remotePort);
+        this.setEncoding('utf8');
 
-			this.on('data', function(data) {
-					console.log(connName + ' From Server: ' + data);
-					});
-			this.on('end', function() {
-					console.log(connName + ' Client disconnected');
-					});
-			this.on('error', function(err) {
-					console.log('Socket Error: ', JSON.stringify(err));
-					});
-			this.on('timeout', function() {
-					console.log('Socket Timed Out');
-					});
-			this.on('close', function() {
-					console.log('Socket Closed');
-					});
-	});
-	return client;
+        this.on('data', function (data) {
+            console.log(connName + ' From Server: ' + data);
+        });
+        this.on('end', function () {
+            console.log(connName + ' Client disconnected');
+        });
+        this.on('error', function (err) {
+            console.log('Socket Error: ', JSON.stringify(err));
+        });
+        this.on('timeout', function () {
+            console.log('Socket Timed Out');
+        });
+        this.on('close', function () {
+            console.log('Socket Closed');
+        });
+    });
+    return client;
 }
 
-function writeData(socket, data){
-	let success = !socket.write(data);
+function writeData(socket, data) {
+    socket.write(data);
 }
 
 let latitude = 0;
 let longitude = 0;
-gps.on('data', function(data) {
-			
-	if (gps.state.lat !== undefined && gps.state.lon !== undefined) {
-				latitude = gps.state.lat.toString(); 
-	            latitude = latitude.substring(0,7);
+gps.on(function () {
 
-				longitude = gps.state.lon.toString(); 
-				longitude = longitude.substring(0,9);
-	}
+    if (gps.state.lat !== undefined && gps.state.lon !== undefined) {
+        latitude = gps.state.lat.toString();
+        latitude = latitude.substring(0, 7);
+
+        longitude = gps.state.lon.toString();
+        longitude = longitude.substring(0, 9);
+    }
 });
 
-setInterval(function() { 
-    let gpsData = {code:'00',lat: latitude, lon: longitude};
-	console.log(gpsData);
-	writeData(node1, JSON.stringify(gpsData));
-},3000);
+setInterval(function () {
+    let gpsData = {code: '00', lat: latitude, lon: longitude};
+    console.log(gpsData);
+    writeData(node1, JSON.stringify(gpsData));
+}, 3000);
 
-port.on('data', function(data) {
-				gps.update(data);
+serialGPS.on('data', function (data) {
+    gps.update(data);
 });
 
 const node1 = getConnection("node1");
